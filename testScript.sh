a3fd6605fc88a9ae77f75aaf47b231ebd5ce3bfd
SteveSquared14 <89590079+SteveSquared14@users.noreply.github.com>
2022-08-17 11:57:35 -0400

    Update testScript.sh

--- testScript.sh
+++ testScript.sh
@@ -1,48 +1,4 @@
 #!/bin/bash
-
-function test(){
-	domain="$1"
-
-	echo "value of domain is $domain"
-
-	#testParam="$(curl -v --silent -s -I "$domain" 2>&1 | grep -E "^Server: ")"
-	testParam="$(curl -sIXGET sky.com | grep -E "Server: ")"
-
-	testArrayParam=($testParam)
-	#value=Apache
-	#test2 $value
-
-	for server in "${!testArrayParam[@]}"; do
-		echo "value is "${testArrayParam[server]}""
-		echo $server
-
-		if [[ "$server" == 1 ]]; then
-			echo "index value is $server"
-			echo "value at index is ${testArrayParam[server]}"
-			
-			paramToSearch="${testArrayParam[server]}"
-			echo "paramToSearch is $paramToSearch"
-
-			if [[ "$paramToSearch" == Apache ]]; then
-				echo "match"
-				searchsploit Apache
-			fi
-			#test2 ${testArrayParam[server]}
-		fi
-	done	
-}
-
-
-
-function test2(){
-
-	echo "in test2()"
-	echo "$1"
-	searchsploit $1
-
-}
-
-
 function test3(){
 	domain="$1"
 	curl -sIXGET https://www.$domain/ | grep -vi content-length >> testCurlFile.txt
